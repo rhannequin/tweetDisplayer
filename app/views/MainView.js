a60d269830ef77b029adf214d18225f96398d47c
RemyHannequin <remy.hannequin@gmail.com>
2012-03-23 18:22:49 +0100

    Parse the status and add links to hashtags, urls & usernames

--- app/views/MainView.js
+++ app/views/MainView.js
@@ -31,9 +31,10 @@ define(["use!backbone", "text!app/views/template.html"], function(Backbone, tmpl
         url: "http://api.twitter.com/1/statuses/show/" + query + '.json',
         dataType: 'jsonp',
         success: function(data) {
+          var text = that.parseTweet(data.text);
           $('#tweet').html(that.tmpl({
             userName: data.user.name,
-            text: data.text
+            text: text
           }));
         }
       });
@@ -42,6 +43,31 @@ define(["use!backbone", "text!app/views/template.html"], function(Backbone, tmpl
     getId: function (str) {
        var array = str.split('/');
       return array[array.length - 1];
+    },
+
+    parseUsername: function(str) {
+      return str.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+        return '<a href="http://twitter.com/' + u.replace("@","") + '">' + u + '</a>';
+      });
+    },
+
+    parseUrl: function(str) {
+      return str.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function(url) {
+        return '<a href="' + url + '">' + url + '</a>';
+      });
+    },
+
+    parseHashtag: function(str) {
+      return str.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+        return '<a href="http://search.twitter.com/search?q=' + t + '">' + t + '</a>';
+      });
+    },
+
+    parseTweet: function(str) {
+      var str = this.parseUrl(str);
+      str = this.parseUsername(str);
+      str = this.parseHashtag(str);
+      return str;
     }
 
   });
